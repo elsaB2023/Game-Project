1820cbef1f8c6b53cbc4432ce8aea05c3657df5d
elsaB2023 <elsabosemark@gmail.com>
2020-11-18 08:31:36 -0800

    Cloud

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,20 +1,11 @@
-
-let b;
-let anotherRain;
-let kite;
 let rain = [];
 
 function setup() {
   createCanvas(800, 400);
-  b = new Rain(0, 100,"red");   
-anotherRain = new Rain(200,20,"green");
-
-
-
 }
 function keyPressed(){ 
 
-  let raindrop = new Rain(random(400),100,"blue");
+let raindrop = new Rain(random(400),100,"blue");
 rain.push(raindrop);
 print(rain);
 }
@@ -23,41 +14,25 @@ print(rain);
 function draw(){
 	background(220);
 
- b.drawRain();
- b.moveRain();
-
- anotherRain.drawRain();
- anotherRain.moveRain();
-
-
- for (let i = 0; i < rain.length; i++){
-  rain[i].drawRain();
-     rain[i].moveRain();
- }
-
 }
-
-
-
-
 
 class Rain {
 
   constructor(x,y,color){ 
      this.x = x;
-         this.y = y;
-            this.color = color;
+     this.y = y;
+     this.color = color;
      }
      drawRain(){ 
-      stroke(0);
+      //noStroke();
       fill(this.color);
       ellipse(this.x,this.y,10,10);
 }
 
 moveRain(){ 
   this.y = this.y+2;
+  }
 }
- }
 
 
 
--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -4,13 +4,16 @@
 let Rains = [];
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(1150, 400);
 
 }
 
 function draw(){
 	background(220);
-
+	translate(0,0)
+	scale(5.0)
+	cloud(15,25,1);
+	cloud(50,25,1);
 //	draw all the Rains in that array
 	for (let i = 0; i < Rains.length; i++) {
 	    Rains[i].drawRain();
@@ -21,7 +24,7 @@ function draw(){
 function keyPressed(){
   // todo: 
   //every time you push a key, make a new Rain from the Rain class and add it to the Rains array
-  let  b = new Rain(100, 100);
+  let  b = new Rain(random(15,75),30);
   Rains.push(b);
   print(Rains);
 
@@ -36,15 +39,21 @@ class Rain {
 	}
 
 	drawRain(){  // draw a Rain on the screen at x,y
-    		stroke(0);
-    		fill("red");
-		    ellipse(this.x,this.y,10,10);
+    		noStroke();
+    		fill("#01c2cb");
+		    ellipse(this.x,this.y,2,2);
 	}
 
 	moveRain(){ //update the location of the Rain, so it moves across the screen
-		this.x = this.x+2;
 		this.y = this.y+.5;
 	}
+}
 
-
+function cloud(x, y, size) {
+	fill(255, 255, 255);
+	noStroke();
+	arc(x, y, 25 * size, 20 * size, PI + TWO_PI, TWO_PI);
+	arc(x + 10, y, 25 * size, 45 * size, PI + TWO_PI, TWO_PI);
+	arc(x + 25, y, 25 * size, 35 * size, PI + TWO_PI, TWO_PI);
+	arc(x + 40, y, 30 * size, 20 * size, PI + TWO_PI, TWO_PI);
 }
